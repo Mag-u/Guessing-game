e8f2521edb8ec3712e60ef27bfd5e38539b647f8
Michał Lipiec <michal.lipiec123@gmail.com>
2021-11-09 20:08:10 +0100

    v1.02

--- Guessing game/Guessing game.cpp
+++ Guessing game/Guessing game.cpp
@@ -8,6 +8,46 @@ int main()
 	int number = 0;
 	int startingNumber = 0;
 	int endingNumber = 0;
+	int copy = 0;
+	int guessingNumber = 0;
+	string stopProgram = "";
+	cout << "Input the starting number: ";
+	cin >> startingNumber;
+	cout << "Input the ending number: ";
+	cin >> endingNumber;
+	cout << endl;
 
-	cout << ""
+	if (startingNumber > endingNumber)															//
+	{																					       //
+		copy = endingNumber;																  //		
+		endingNumber = startingNumber;                                                       //
+		startingNumber = copy;                                                                //
+	}                                                                                          //
+	else if (startingNumber == endingNumber)                                                    //
+	{                                                                                            //
+		cout << "The numbers are that same and becouse of it the game can't start" << endl;        //              AS FUNCTION
+		cout << "Do you want to input numbers again? Y/N";                                       //
+		cin >> stopProgram;                                                                    //
+		if (stopProgram != "Y")                                                                //
+		{                                                                                       //
+			exit(0);                                                                             //
+		}                                                                                         //
+	}																							   //
+
+	cout << "Input the number of chances for guess: ";
+	cin >> chances;
+	number = rand() % endingNumber + startingNumber;
+	cout << endl << "Start guessing: ";
+	while (guessingNumber != number)
+	{
+		cout << "You have " << chances << " lives " << endl;
+		cin >> guessingNumber;
+		chances--;
+		if (chances == 0)
+		{
+			cout << "You lost";
+			cout << "The number was: " << number;
+			exit(0);
+		}
+	}
 }
