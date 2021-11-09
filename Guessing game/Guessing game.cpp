9f4bf0f5f2b515ec6efb7f35fba780e41f6e2d12
Michał Lipiec <michal.lipiec123@gmail.com>
2021-11-09 19:15:48 +0100

    v1.01

--- Guessing game/Guessing game.cpp
+++ Guessing game/Guessing game.cpp
@@ -1,7 +1,13 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-   cout << "Hello World!\n";
+	int chances = 0;
+	int number = 0;
+	int startingNumber = 0;
+	int endingNumber = 0;
+
+	cout << ""
 }
